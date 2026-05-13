2db01aeb4846a345c218583d6417d804c5dda74d
AtramAniket <aniket_atram@live.in>
2026-05-13 14:36:38 +0530

    created app factory for application

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from config import Config
+from app.extensions import db, migrate, login_manager
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    from app.routes.main import main_bp
+    from app.routes.auth import auth_bp
+
+    from app.models.user import User
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    return app
