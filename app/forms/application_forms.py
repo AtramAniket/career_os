1de851f3fb30513785a4479bdbf528be59c3e4c6
AtramAniket <aniket_atram@live.in>
2026-05-14 14:48:55 +0530

    added missing fields from table to form

--- app/forms/application_forms.py
+++ app/forms/application_forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField
+from wtforms import StringField, TextAreaField, IntegerField, SelectField, DateField, SubmitField
 from wtforms.validators import DataRequired, Optional, Length
 
 class JobApplicationForm(FlaskForm):
@@ -24,22 +24,67 @@ class JobApplicationForm(FlaskForm):
 		validators=[Optional(), Length(max=120)]
 		)
 
-	status = SelectField(
-		"Status",
+	work_mode = SelectField(
+		"Work Mode",
 		validators=[DataRequired()],
 		choices=[
-		("saved", "Saved"),
-		("applied", "Applied"),
-		("interviewing", "Interviewing"),
-		("offer", "Offer"),
-		("rejected", "Rejected"),
-		("archived", "Archived")]
+		("REMOTE", "Remote"),
+		("HYBRID", "Hybrid"),
+		("ONSITE", "On-Site")]
 		)
+
+	employment_type = SelectField(
+		"Employment Type",
+		validators=[DataRequired()],
+		choices=[
+		("FULL_TIME", "Full-Time"),
+		("PART_TIME", "Part-Time"),
+		("INTERNSHIP", "Internship"),
+		("CONTRACT", "Contract")]
+		)
+
+	salary_min = IntegerField(
+		"Minimum Expected Salary",
+		validators=[Optional()])
+
+	salary_max = IntegerField(
+		"Maximum Expected Salary",
+		validators=[Optional()])
 
 	source = StringField(
 		"Source",
 		validators=[Optional(), Length(max=120)]
 		)
+
+	status = SelectField(
+		"Status",
+		validators=[DataRequired()],
+		choices=[
+		("SAVED", "Saved"),
+		("APPLIED", "Applied"),
+		("ASSESSMENT", "Assessment"),
+		("INTERVIEW", "Interview"),
+		("OFFER", "Offer"),
+		("REJECTED", "Rejected"),
+		("GHOSTED", "Ghosted")]
+		)
+
+	priority = SelectField(
+		"Priority",
+		validators=[DataRequired()],
+		choices=[
+		("LOW", "Low"),
+		("MEDIUM", "Medium"),
+		("HIGH", "High"),]
+		)
+
+	date_applied = DateField(
+		"Applied On",
+		validators=[Optional()])
+
+	deadline = DateField(
+		"Application Deadline",
+		validators=[Optional()])
 
 	notes = TextAreaField(
 		"Notes",
