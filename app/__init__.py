22e743d8b3acccd3f58b944fa25f9ef7ce325340
AtramAniket <aniket_atram@live.in>
2026-05-14 14:48:26 +0530

    registered job application routes in feature store

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,12 @@ def create_app(config_class=Config):
 
     from app.routes.main import main_bp
     from app.routes.auth import auth_bp
+    from app.routes.applications import applications_bp
 
     from app import models
 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(applications_bp)
 
     return app
