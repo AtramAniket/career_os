ad239c7769bc051046170bb9d59bac95d16493f9
AtramAniket <aniket_atram@live.in>
2026-05-19 15:38:34 +0530

    fixed column names

--- app/routes/applications.py
+++ app/routes/applications.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 
 from app.extensions import db
-from app.forms.application_forms import JobApplicationForm
-from app.models.job_application import JobApplication
+from app.forms.application_forms import JobApplicationForm, ApplicationEventForm
+from app.models import JobApplication, ApplicationEvent
 
 applications_bp = Blueprint("applications", __name__, url_prefix="/applications")
 
@@ -57,4 +57,44 @@ def create():
 	return render_template(
 		"applications/create.html",
 		form=form
+		)
+
+
+@applications_bp.route("/<int:application_id>", methods=["GET", "POST"])
+@login_required
+def detail(application_id):
+
+	application = (JobApplication.query\
+		.filter_by(id=application_id, user_id=current_user.id)\
+		.first_or_404())
+
+	form = ApplicationEventForm()
+
+	if form.validate_on_submit():
+		event = ApplicationEvent(
+			job_application_status = application_id,
+			title = form.title.data,
+			event_date = form.event_date.data,
+			description = form.description.data,
+			event_type = form.event_type.data,
+		)
+
+		db.session.add(event)
+		db.session.commit()
+
+		flash("Application event added successfully", "success")
+		return redirect(url_for("applications.detail", application_id=application.id))
+
+	events = (
+		ApplicationEvent.query\
+		.filter_by(job_application_status = application.id)\
+		.order_by(ApplicationEvent.event_date.desc())\
+		.all()
+		)
+
+	return render_template(
+		"applications/detail.html",
+		form=form,
+		events=events,
+		application=application
 		)
