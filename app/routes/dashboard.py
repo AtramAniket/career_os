b0d60ff120d631683c30b7ea6beefc52ff093dde
AtramAniket <aniket_atram@live.in>
2026-05-21 14:51:16 +0530

    added route for dashboard

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+
 from sqlalchemy import func, desc
 
 from app.extensions import db
@@ -28,20 +29,27 @@ def index():
     recent_applications = JobApplication.query\
     .filter_by(user_id=current_user.id, is_deleted=False)\
     .order_by(desc(JobApplication.created_at))\
-    .limit(5)\
+    .limit(8)\
     .all()
 
     recent_events = ApplicationEvent.query\
     .join(JobApplication)\
     .filter_by(user_id=current_user.id, is_deleted=False)\
-    .order_by(desc(JobApplication.created_at))\
-    .limit(5)\
+    .order_by(desc(ApplicationEvent.created_at), desc(ApplicationEvent.created_at))\
+    .limit(8)\
     .all()
 
+    interview_count = counts.get("Interview", 0)
+    closed_count = counts.get("Rejected", 0) + counts.get("Ghosted", 0) + counts.get("Offer", 0)
+    active_count = counts.get("Saved", 0) + counts.get("Applied", 0) + counts.get("Assessment", 0)
+   
     return render_template(
         "dashboard.html",
-        total_applications=total_applications,
         counts=counts,
-        recent_applications=recent_applications,
+        JobStatus=JobStatus,
+        active_count=active_count,
+        closed_count=closed_count,
         recent_events=recent_events,
-        JobStatus=JobStatus)
+        interview_count=interview_count,
+        total_applications=total_applications,
+        recent_applications=recent_applications)
