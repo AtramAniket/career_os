ee9c646e9ebcc55899346f34324cac0db73546ce
AtramAniket <aniket_atram@live.in>
2026-05-22 14:59:39 +0530

    added base dir and removed dev key from config file

--- config.py
+++ config.py
@@ -5,8 +5,10 @@ load_dotenv()
 
 class Config:
 
-	SECRET_KEY = os.getenv('SECRET_KEY', '4526bed2012138f5930bd7c7bdd24b6b07ad8a14c7894407e8f4aaf547e3152448413707165fb2d0963265d5d6310eff3674269427975181')
+	SECRET_KEY = os.getenv('SECRET_KEY')
 
 	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///career_os.db')
 
 	SQLALCHEMY_TRACK_NOTIFICATIONS = False
+
+	BASE_DIR = os.path.abspath(os.path.dirname(__file__))
