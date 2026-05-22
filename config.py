fb077792c823d128cfe9acd9240d424d88de789b
AtramAniket <aniket_atram@live.in>
2026-05-22 15:00:36 +0530

    added based directory folder location and max file upload size

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
 
 	SECRET_KEY = os.getenv('SECRET_KEY')
@@ -11,4 +13,6 @@ class Config:
 
 	SQLALCHEMY_TRACK_NOTIFICATIONS = False
 
-	BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+	UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+
+	MAX_CONTENT_LENGTH = 16 * 1024 * 1024
