dce3525778881f05c8b532213a7bce2aebe113f1
AtramAniket <aniket_atram@live.in>
2026-05-22 13:31:06 +0530

    added newly added main route to feature store

--- app/__init__.py
+++ app/__init__.py
@@ -11,14 +11,16 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login_manager.init_app(app)
 
-    from app.routes.dashboard import dashboard_bp
+    from app.routes.main import main_bp
     from app.routes.auth import auth_bp
+    from app.routes.dashboard import dashboard_bp
     from app.routes.applications import applications_bp
 
     from app import models
 
-    app.register_blueprint(dashboard_bp)
+    app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(dashboard_bp)
     app.register_blueprint(applications_bp)
 
     return app
