1af304d6c9381200cd92be2c3ed27334bc8e70ac
AtramAniket <aniket_atram@live.in>
2026-05-25 14:37:08 +0530

    added front-end for resume analysis

--- app/routes/resume_analysis.py
+++ app/routes/resume_analysis.py
@@ -1,7 +1,7 @@
 from flask_login import login_required, current_user
 from flask import Blueprint, url_for, render_template, redirect, abort, flash
 
-from app import models
+from app.models import ApplicationDocument, JobApplication
 from app.extensions import db
 
 resume_analysis_bp = Blueprint("resume_analysis", __name__, url_prefix='/applications')
@@ -16,7 +16,7 @@ def analyze(application_id):
 	if application.user_id != current_user.id:
 		abort(404)
 
-	primary_resume = JobApplication.query\
+	primary_resume = ApplicationDocument.query\
 	.filter_by(
 		job_application_id=application.id,
 		document_type="resume",
@@ -24,14 +24,14 @@ def analyze(application_id):
 		)\
 	.first()
 
-	if not application.job_description:
+	if not application.job_descritpion:
 		flash("Add a job description before running analysis", "warning")
-		return redirect(url_for("applications.details", application_id=application.id))
+		return redirect(url_for("applications.detail", application_id=application.id))
 
 	if not primary_resume:
 		flash("Please set a primary resume before running analysis", "warning")
-		return redirect(url_for("applications.details", application_id=application.id))
+		return redirect(url_for("applications.detail", application_id=application.id))
 
 	flash("Resume analysis foundation is ready. AI analysis will be connected later", "info")
-	return redirect(url_for("applications.details", application_id=application.id))
+	return redirect(url_for("applications.detail", application_id=application.id))
 
