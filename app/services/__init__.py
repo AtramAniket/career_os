ecf1bd5e90172e3dcf87d1075ff48c18f1509446
AtramAniket <aniket_atram@live.in>
2026-05-27 15:06:49 +0530

    fixed circular import

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,9 +1,7 @@
 from .resume_analysis_pipeline import run_resume_analysis
-from .resume_analysis_service import analyze_resume_with_ai
-from .resume_tailoring_service import generate_tailoring_suggestions
+from .resume_tailoring_pipeline import run_resume_tailoring
 
 __all__ = [
 "run_resume_analysis",
-"analyze_resume_with_ai",
-"generate_tailoring_suggestions",
+"run_resume_tailoring",
 ]
