1aee2123b30f4321d1ec7cd407d84479a5087f08
AtramAniket <aniket_atram@live.in>
2026-05-27 14:12:49 +0530

    added run_resume_analysis to package initializer

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,7 @@
 from app.services.resume_analysis_service import analyze_resume_with_ai
+from app.services.resume_analysis_pipeline import run_resume_analysis
 
 __all__ = [
-"analyze_resume_with_ai"
+"analyze_resume_with_ai",
+"run_resume_analysis"
 ]
