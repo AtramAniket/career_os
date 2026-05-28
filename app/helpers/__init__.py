69ce6b63de12e98e403531c888426bb80964ca6a
AtramAniket <aniket_atram@live.in>
2026-05-28 15:06:47 +0530

    added helper for AI interview prep

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,7 +1,9 @@
 from app.helpers.document_upload_helper import allowed_document_file
 from app.helpers.resume_analysis_helper import extract_text_from_pdf
+from app.helpers.interview_prep_helper import build_interview_prep_prompt
 
 __all__ = [
 "allowed_document_file",
-"extract_text_from_pdf"
+"extract_text_from_pdf",
+"build_interview_prep_prompt"
 ]
