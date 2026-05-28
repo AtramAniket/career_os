fa65e819cfad3f90f4eabc1ef294d1747d610e00
AtramAniket <aniket_atram@live.in>
2026-05-28 12:55:32 +0530

    added config for postgres db

--- config.py
+++ config.py
@@ -5,11 +5,17 @@ load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+db_url = os.environ.get("DATABASE_URL", "sqlite:///career_os.db")
+
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+
 class Config:
 
 	SECRET_KEY = os.getenv('SECRET_KEY')
 
-	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///career_os.db')
+	SQLALCHEMY_DATABASE_URI = os.getenv(db_url)
 
 	SQLALCHEMY_TRACK_NOTIFICATIONS = False
 
